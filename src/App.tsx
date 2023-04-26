d2885e1638fef358af922d3445e43ac37e1b983d
Nico Loos <nico.loos@slicr.de>
2023-04-26 19:12:56 +0200

    JE-27: Make Sidebar Slideable

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,65 @@
 import './App.css'
 
+import SplitPane from './components/split-pane.component'
 import Sidebar from './components/sidebar.component'
 import Main from './components/main.component'
 
 function App() {
-  return (
-    <div className="row h-100" style={{ backgroundColor: '#1e1e1e' }}>
-      <div className="col-2 pe-1 d-flex flex-column">
-        <div className="ms-2 my-2 bg-dark border flex-grow-1">
+  const Left = (
+    <>
+      <div
+        className="col-3 pe-1 d-flex flex-column w-100"
+        style={{ height: '100vh' }}
+      >
+        <div
+          className="ms-2 my-2 border flex-grow-1"
+          style={{ backgroundColor: '#2D2B55' }}
+        >
           <Sidebar />
         </div>
       </div>
-      <div className="col-10 ps-1 d-flex flex-column">
+    </>
+  )
+
+  const Right = (
+    <>
+      <div
+        className="col-9 ps-1 d-flex flex-column w-100"
+        style={{ height: '100vh' }}
+      >
         <div className="me-2 my-2 bg-dark border flex-grow-1">
           <Main />
         </div>
       </div>
+    </>
+  )
+
+  return (
+    <div className="row h-100 app">
+      <SplitPane initialLeftWidth={300} left={Left} right={Right} />
     </div>
   )
+
+  /*return (
+    <div className="row h-100" style={{ backgroundColor: '#1e1e1e' }}>
+      <SplitView 
+        left={(
+          <div className="col-3 pe-1 d-flex flex-column w-100" style={{ height: '100vh' }}>
+            <div className="ms-2 my-2 bg-dark border flex-grow-1">
+              <Sidebar />
+            </div>
+          </div>
+        )}
+        right={(
+          <div className="col-9 ps-1 d-flex flex-column w-100" style={{ height: '100vh' }}>
+            <div className="me-2 my-2 bg-dark border flex-grow-1">
+              <Main />
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  )*/
 }
 
 export default App
---
+++ src/components/split-pane.component.tsx
@@ -0,0 +1,100 @@
+import './split-pane.component.css'
+
+import React, {
+  createRef,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
+import SplitPaneContext from '../context/split-pane.context'
+
+type Props = {
+  initialLeftWidth: number
+  left: JSX.Element | JSX.Element[]
+  right: JSX.Element | JSX.Element[]
+}
+
+const Divider = () => {
+  const { onMouseHoldDown } = useContext(SplitPaneContext)!
+
+  return <div className="separator-col" onMouseDown={onMouseHoldDown as any} />
+}
+
+const SplitPaneLeft = (props: React.PropsWithChildren) => {
+  const topRef = createRef<HTMLDivElement>()
+  const { clientWidth, setClientWidth } = useContext(SplitPaneContext)!
+
+  useEffect(() => {
+    if (topRef.current) {
+      if (!clientWidth) {
+        setClientWidth(topRef.current.clientWidth / 2)
+        return
+      }
+
+      topRef.current.style.minWidth = clientWidth + 'px'
+      topRef.current.style.maxWidth = clientWidth + 'px'
+    }
+  }, [clientWidth])
+
+  return (
+    <div className="split-pane-left" ref={topRef}>
+      {props.children}
+    </div>
+  )
+}
+
+const SplitPaneRight = (props: React.PropsWithChildren) => {
+  return <div className="split-pane-right">{props.children}</div>
+}
+
+const SplitPane = (props: Props) => {
+  const [clientWidth, setClientWidth] = useState<number>(props.initialLeftWidth)
+  const xDividerPos = useRef<number | null>(null)
+
+  const onMouseHoldDown = (e: MouseEvent) => {
+    xDividerPos.current = e.clientX
+  }
+
+  const onMouseHoldUp = () => {
+    xDividerPos.current = null
+  }
+
+  const onMouseHoldMove = (e: MouseEvent) => {
+    if (!xDividerPos.current) {
+      return
+    }
+
+    setClientWidth(clientWidth! + e.clientX - xDividerPos.current!)
+
+    xDividerPos.current = e.clientX
+  }
+
+  useEffect(() => {
+    document.addEventListener('mouseup', onMouseHoldUp)
+    document.addEventListener('mousemove', onMouseHoldMove)
+
+    return () => {
+      document.removeEventListener('mouseup', onMouseHoldUp)
+      document.removeEventListener('mousemove', onMouseHoldMove)
+    }
+  })
+
+  return (
+    <div className="split-pane-row">
+      <SplitPaneContext.Provider
+        value={{
+          clientWidth,
+          setClientWidth,
+          onMouseHoldDown,
+        }}
+      >
+        <SplitPaneLeft>{props.left}</SplitPaneLeft>
+        <Divider />
+        <SplitPaneRight>{props.right}</SplitPaneRight>
+      </SplitPaneContext.Provider>
+    </div>
+  )
+}
+
+export default SplitPane
